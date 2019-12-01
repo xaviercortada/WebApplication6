e9d07b9eeb427332cfd922d15ad2fcca399171ba
Xavier Cortada Coromina <xavier.cortada@yahoo.es>
2019-12-01 17:06:55 +0100

    Agregar archivos de proyecto.

---
+++ WebApiTest/App_Start/Startup.Auth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using Microsoft.IdentityModel.Tokens; 
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+
+namespace WebApiTest
+{
+    public partial class Startup
+    {
+        // Para obtener más información sobre cómo configurar la autenticación, visite https://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseActiveDirectoryFederationServicesBearerAuthentication(
+                        new ActiveDirectoryFederationServicesBearerAuthenticationOptions
+                        {
+                            MetadataEndpoint = ConfigurationManager.AppSettings["ida:AdfsMetadataEndpoint"],
+                            TokenValidationParameters = new TokenValidationParameters()
+                            {
+                                ValidAudience = ConfigurationManager.AppSettings["ida:Audience"]
+                                // ValidIssuer = ConfigurationManager.AppSettings["ida:Issuer"]
+                            }
+                        }
+             );
+        }
+    }
+}
---
+++ WebApiTest/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace WebApiTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
